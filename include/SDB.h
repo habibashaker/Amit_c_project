d73550bfe3eecb731a6d45d9cb8a7c5e1a0aa6c4
habibashaker <habibashaker25@gmail.com>
2025-07-27 20:14:38 +0300

    Setup complete + header files created

---
+++ include/SDB.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "STD.h"
+
+typedef struct SimpleDb
+{
+    uint32 Student_ID;
+    uint32 Student_year;
+    uint32 Course1_ID;
+    uint32 Course1_grade;
+    uint32 Course2_ID;
+    uint32 Course2_grade;
+    uint32 Course3_ID;
+    uint32 Course3_grade;
+}student;
+
+Bool SDB_IsFull();
+uint8 SDB_GetUsedSize();
+Bool SDB_AddEntry();
+void SDB_DeleteEntry(uint32 id);
+Bool SDB_ReadEntry(uint32 id);
+void SDB_GetList(uint8 * count,uint32 * list);
+Bool SDB_IsIdExist(uint32 id);
+
---
+++ include/STD.h
@@ -0,0 +1,10 @@
+#ifndef STD_H
+#define STD_H
+typedef unsigned char uint8;
+typedef char int8;
+typedef unsigned short int uint16;
+typedef short int int16;
+typedef unsigned int uint32;
+typedef int int32;
+typedef enum {False,True} Bool;
+#endif
